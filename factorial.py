4e688639092b9be2fd638fd7f63638871bfc6fd7
Krutarth Patel <kr739904@dal.ca>
2020-02-27 16:56:11 -0400

    Factorial program added!

---
+++ factorial.py
@@ -0,0 +1,21 @@
+import sys
+import csv
+import re
+import string
+
+with open("Input.txt",encoding='utf8',newline='') as obj:
+   read = csv.reader(obj)
+   st=0
+   for row in read:
+      num=int(row[0])
+      
+      factorial = 1
+
+      if num < 0:
+         print("Sorry, factorial does not exist for negative numbers")
+      elif num == 0:
+         print("The factorial of 0 is 1")
+      else:
+         for i in range(1,num + 1):
+             factorial = factorial*i
+         print("The factorial of",num,"is",factorial)
