3ba76f1d4159e0a7191aeaf436e486f8ab6bbb32
Krutarth Patel <kr739904@dal.ca>
2020-02-28 18:04:40 -0400

    Range of random number generator increased to 100

--- InputFileGenerator.py
+++ InputFileGenerator.py
@@ -4,8 +4,8 @@ import csv
 fall= open('Input.txt', 'w', encoding="utf-8",newline='')
 wrall = csv.writer(fall)
 
-for i in range(0,20):
+for i in range(0,100):
     l=[]
-    l.append(random.randint(0,9))
+    l.append(random.randint(0,100))
     wrall.writerow(l)
 
