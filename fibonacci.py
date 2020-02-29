898e58a69e602abe1226a9fba91a145c6c4c7bc0
Krutarth Patel <krutarth07@gmail.com>
2020-02-28 20:00:32 -0400

    Added the graph plot functionality

--- fibonacci.py
+++ fibonacci.py
@@ -2,23 +2,30 @@ import sys
 import csv
 import re
 import string
+import time
+#import matplotlib.pyplot as plt
+
 
 with open("Input.txt",encoding='utf8',newline='') as obj:
    read = csv.reader(obj)
+   st=0
+   ans=""
+   n=[]
+   t=[]
    for row in read:
+      start = time.clock()
+      ans=ans+"Request ID: "+str(st)+", "
+      st+=1
+      timeDel=0.00003
       num=int(row[0])
 
-      print("Fibonacci series for n = "+str(num)+" :")
       a = 0
       b = 1
       ser=""
-      if num < 0: 
-         print("Incorrect input") 
-      elif num == 0:
-         print(a)
+      if num == 0:
+         ser+=str(a)+" "
       elif num == 1:
-         print(a)
-         print(b)
+         ser+=str(a)+" "+str(b)+" "
       else:
          ser+="0 1"
          for i in range(2,num): 
@@ -26,4 +33,11 @@ with open("Input.txt",encoding='utf8',newline='') as obj:
             a = b
             b = c
             ser+=" "+str(b)
-      print(ser)
+      end = time.clock()
+      if(st!=1 and str(num) not in n):
+         n.append(str(num))
+         t.append(end-start)
+      ans+="Start: "+str(start)+", End: "+str(end)+", Duration: "+str((end-start))+", N: "+str(num)+"\nOutput: "+ser+"\n\n"
+print(ans)
+#plt.plot(n,t)
+#plt.show()
